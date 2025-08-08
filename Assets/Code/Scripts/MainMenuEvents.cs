fc75420331d9917e4e66d1ce9419f7b769737c1b
K-a-a-m <yduchateau@gmail.com>
2025-08-08 14:23:48 +0200

    Title Screen started + Idle

---
+++ Assets/Code/Scripts/MainMenuEvents.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuEvents : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public void StartGame()
+    {
+        Debug.Log("START GAME");
+    }
+}
