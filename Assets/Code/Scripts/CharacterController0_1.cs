a3c31fded94c3cc8d3647a4ad60c0473e97d725d
proboz <thomasrety@gmail.com>
2025-08-08 10:39:19 +0200

    TestMap + Jump update

--- Assets/Code/Scripts/CharacterController0_1.cs
+++ Assets/Code/Scripts/CharacterController0_1.cs
@@ -28,7 +28,7 @@ public class CharacterController0_1 : MonoBehaviour
 
     private InputAction interactionAction;
 
-    private int nbJumpsLeft = 0; 
+    public int nbJumpsLeft = 0; 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
