2c867547ca27548902cb1aab89941bce3d7bb72e
proboz <thomasrety@gmail.com>
2025-08-08 19:23:02 +0200

    Update Audio shake and rotate

--- Assets/Code/Scripts/CharacterController0_1.cs
+++ Assets/Code/Scripts/CharacterController0_1.cs
@@ -19,9 +19,7 @@ public class CharacterController0_1 : MonoBehaviour
     [SerializeField] private GameObject bounderyDown;
     [SerializeField] private int targetOrbes;
     [SerializeField] private AudioClip JumpSFX;
-    [SerializeField] private AudioClip LandSFX;
     [SerializeField] private AudioClip OrbeSFX;
-    [SerializeField] private AudioClip RotationSFX;
     [SerializeField] private AudioClip MarcheSFX;
     // private float horizontal = -Input.GetAxisRaw("Horizontal"); // Inverse gauche/droite
 
@@ -145,7 +143,9 @@ public class CharacterController0_1 : MonoBehaviour
             nbJumpsLeft--;
 
             audioSource.PlayOneShot(JumpSFX);
+
         }
+        
     }
 
     private void MoveCharacter(int moveDir)
