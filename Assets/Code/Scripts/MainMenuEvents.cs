83e049fd435ed45026c8c942ccf9e21ad60a9a63
K-a-a-m <yduchateau@gmail.com>
2025-08-08 15:50:31 +0200

    Loadscene added

--- Assets/Code/Scripts/MainMenuEvents.cs
+++ Assets/Code/Scripts/MainMenuEvents.cs
@@ -6,9 +6,10 @@ public class MainMenuEvents : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
-    public void StartGame()
+    public void StartGame(int sceneNumber)
     {
         Debug.Log("START GAME");
+        SceneManager.LoadScene(sceneNumber);
     }
 
     public void ShowCredits()
@@ -19,5 +20,6 @@ public class MainMenuEvents : MonoBehaviour
     public void QuitGame()
     {
         Debug.Log("QUIT GAME");
+        //Application.Quit();
     }
 }
