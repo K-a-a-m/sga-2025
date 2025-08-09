446eb46df1cb1cffd100fa65457c88c775794c3f
K-a-a-m <yduchateau@gmail.com>
2025-08-09 12:24:17 +0200

    Palette Added

--- Assets/Code/Scripts/OrbesControl.cs
+++ Assets/Code/Scripts/OrbesControl.cs
@@ -1,19 +1,32 @@
 using System.Collections;
 using UnityEngine;
 
+
 public class OrbesControl : MonoBehaviour
 {
     [SerializeField] private GameObject playerController;
     [SerializeField] private  OrbeMusicConttroller orbParent;
     [SerializeField] private GameObject orbParentGO;
+    [SerializeField] private GameObject orbUI;
+
+    private Vector3 amount = new Vector3(.5f, .5f, .5f);
+
+
+    private float time = 1.5f;
     private Collider2D colliderTrigger;
     private CharacterController0_1 characterController;
     private bool hasDetect = true;
-
+    
     private void Start()
     {
         characterController = playerController.GetComponent<CharacterController0_1>();
         colliderTrigger = GetComponent<Collider2D>();
+        float randomTime = Random.Range(time - 0.5f, time + 0.5f);
+        iTween.PunchScale(gameObject, iTween.Hash(
+            "amount", amount,
+            "time", randomTime,
+            "looptype", iTween.LoopType.loop
+        ));
     }
     private void OnTriggerEnter2D(Collider2D colliderTrigger)
     {
@@ -23,7 +36,7 @@ public class OrbesControl : MonoBehaviour
             characterController.orbesNumber++;
             characterController.checkPoint = true;
             orbParent.ChangeAudioClip();
-            
+            orbUI.SetActive(true);
             Destroy(gameObject);
             //orbParentGO.SetActive(false);
             StartCoroutine(DisableOrbParent());
