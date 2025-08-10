2089f6e1c9a49bec7319980f61f016f72e6f8ad3
K-a-a-m <yduchateau@gmail.com>
2025-08-11 00:21:09 +0200

    FinalScreenLayered created with animation of layers

--- Assets/Code/Scripts/Dialog/DialogManagerEnd.cs
+++ Assets/Code/Scripts/Dialog/DialogManagerEnd.cs
@@ -1,20 +1,30 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class DialogManagerEnd : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI dialogTextEnd;
     [SerializeField] GameObject dialogContainerPanel;
+    [SerializeField] GameObject dialogSpeakerPanel;
+    [SerializeField] Image dialogSpeakerImage;
+    [SerializeField] Image dialogTextImage;
     [SerializeField] TextMeshProUGUI speakerText;
     [SerializeField] private GameObject butterflies;
     [SerializeField] private GameObject canvasBack;
+    [SerializeField] private List<Animator> bgAnimators;
+    [SerializeField] private int indexReveal = 2;
+    [SerializeField] private float revealDuration = 3.2f;
+    [SerializeField] private Color32 colorGreen;
+    [SerializeField] private Color colorYellow = new Color(0.8f , 0.7490196f , 0.3921569f, 1f); //= new Color(204,191,100);
     int dialogIndex = 0;
     public int currentDialog = 1;
-
+    
         List<Tuple<string, string>> dialogFin = new List<Tuple<string, string>>()
         {
         //new Tuple<string,string> ("Super, j�ai retrouv� mon pinceau magique.", "Milo"),
@@ -50,15 +60,30 @@ public class DialogManagerEnd : MonoBehaviour
         if(nextDialog.WasPressedThisFrame())
         {
             string speaker = "";
-            dialogContainerPanel.SetActive(true);
+            if (!dialogContainerPanel.activeInHierarchy) return;
+            
             switch(currentDialog)
             {
                 case 1: 
+                    
                     if (dialogIndex < dialogFin.Count)
                     {
+                        
                         dialogTextEnd.text = dialogFin[dialogIndex].Item1;
                         speaker = dialogFin[dialogIndex].Item2;
                         dialogIndex++;
+                        if (speaker == String.Empty)
+                            dialogSpeakerPanel.SetActive(false);
+                        else
+                        {
+                            dialogSpeakerPanel.SetActive(true);
+                        }
+                            
+                        if (dialogIndex == indexReveal)
+                        {
+                            dialogContainerPanel.SetActive(false);
+                            StartCoroutine(RevealBackground());
+                        }
                     }
                     else
                     {
@@ -70,7 +95,7 @@ public class DialogManagerEnd : MonoBehaviour
                         
                     }
                     break;
-            default:
+                default:
                     dialogContainerPanel.SetActive(false);
                     speaker = String.Empty;
                     currentDialog=0;
@@ -85,5 +110,17 @@ public class DialogManagerEnd : MonoBehaviour
     public void OnClickBackButton()
     {
         SceneManager.LoadScene("Credit");
+    }
+
+    IEnumerator RevealBackground()
+    {
+        foreach (Animator anim in bgAnimators)
+        {
+            anim.SetTrigger("Reveal");
+        }
+        yield return new WaitForSeconds(revealDuration);
+        dialogSpeakerImage.color = colorYellow;
+        dialogTextImage.color = colorYellow;
+        dialogContainerPanel.SetActive(true);
     }
 }
